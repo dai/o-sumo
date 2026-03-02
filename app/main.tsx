9d57266e16501d63a322572b4d16890dfd16b355
dai <dosada@gmail.com>
2026-03-02 16:08:54 +0900

    feat: 取組表ページ追加と力士名英語表記の導入

--- app/main.tsx
+++ app/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './page'
 import BanzukePage from './202603-o-sumo/page'
+import TorikumiPage from './202603-torikumi/page'
 import './globals.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/202603-o-sumo" element={<BanzukePage />} />
+        <Route path="/202603-torikumi" element={<TorikumiPage />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
