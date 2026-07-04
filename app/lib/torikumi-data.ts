698c61db9ed28c532084eca2065f398028636608
dai <12391+dai@users.noreply.github.com>
2026-07-04 07:28:09 +0000

    chore: daily sumo data update

--- app/lib/torikumi-data.ts
+++ app/lib/torikumi-data.ts
@@ -59,9 +59,9 @@ export interface TorikumiDataSet {
 export const torikumiData: TorikumiDataSet = {
   "bashoName": "七月場所",
   "year": "令和八年",
-  "updatedAt": "2026-07-04T13:43:31+09:00",
+  "updatedAt": "2026-07-04T16:28:07+09:00",
   "resultUpdatedAt": "2026-06-29T16:26:01+09:00",
-  "scheduleUpdatedAt": "2026-07-04T13:43:31+09:00",
+  "scheduleUpdatedAt": "2026-07-04T16:28:07+09:00",
   "today": null,
   "tomorrow": {
     "makuuchi": {
